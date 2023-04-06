f0fe886dab7cff52891a5e43056dafd7751d3e75
Ojani Figueroa <ojanifigueroa.of@gmail.com>
2023-04-06 18:12:30 -0400

    A stats box now appears for the last 4 games played in StatsState.

--- src/Game/States/StatsState.cpp
+++ src/Game/States/StatsState.cpp
@@ -26,10 +26,47 @@ void StatsState::renderAllTimeStats() {
 	gameFont10px.draw("Time played: " + Stats::getTimePlayed(gameData->getTimePlayed()), 20, 310);
 }
 
+void StatsState::renderStatsInPage() {
+	// Checking if ther are any stats to render
+	if(pages < 1) {
+		gameFont14px.draw("No games have been played", ofGetViewportWidth() / 2.0 - 25 * 9.5, 520);
+		return;
+	}
+
+	// Getting first iterator of the current page
+	int beginAt = (currentPage - 1) * 4;
+	auto iterator = gameData->getAllStats().rbegin() + beginAt;
+
+	// Entries in page is always 4 unless you are in the last page,
+	// in which case the number of entries here is: (total_entries % 4)
+	// When (total_entries % 4) == 0, it means that the last page is full, therefore
+	// The number of entries is set to 4.
+	int entriesInPage = pages > currentPage? 4 : numberOfStats % 4;
+	if(entriesInPage == 0) entriesInPage = 4;
+
+	int i = 0;
+
+	// Iterating through the stats of the current page stats
+	for(auto it = iterator; it != iterator + entriesInPage; it++) {
+		int width = 387;
+		int height = 170;
+		int padding = 15;
+
+		int xPos = i % 2 == 0? padding : padding*2 + width;
+		int yPos = i < 2? 387 : 387 + padding + height;
+
+		ofSetColor(ofColor::antiqueWhite);
+		ofDrawRectRounded(xPos, yPos, width, height, 10);
+
+		i++;
+	}
+}
+
 void StatsState::render() {
 	ofSetBackgroundColor(100);
 	header.render();
 	renderAllTimeStats();
+	renderStatsInPage();
 }
 
 void StatsState::keyPressed(int key){
@@ -44,4 +81,9 @@ void StatsState::reset(){
 	setFinished(false);
 	setNextState("");
 	menuButton->reset();
+
+	// Setting up stats pages
+	numberOfStats = gameData->getAllStats().size();
+	pages = ceil(numberOfStats / 4.0);
+	currentPage = pages > 0? 1 : 0;
 }
--- src/Game/States/StatsState.h
+++ src/Game/States/StatsState.h
@@ -13,12 +13,20 @@ private:
 	GameFont gameFont10px;
 	GameFont gameFont14px = GameFont(14);
 
-public:
-	StatsState(GameData *gameData);
-	void tick();
-	void render();
-	void keyPressed(int key);
-	void mousePressed(int x, int y, int button);
-	void reset();
+	// Pages is the number of pages of stats. each page can hold up to 4 stats entries.
+	// So the number of pages is : ⌊number_of_entries / 4⌋ (the ceiling of # of entries / 4)
+	int pages = 0;
+	int currentPage = 0;
+	int numberOfStats = 0;
+
 	void renderAllTimeStats();
+	void renderStatsInPage();
+
+	public:
+		StatsState(GameData *gameData);
+		void tick();
+		void render();
+		void keyPressed(int key);
+		void mousePressed(int x, int y, int button);
+		void reset();
 };
